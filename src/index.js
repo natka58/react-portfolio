51dc746ccc607d099cefcd3500624faf5780da2e
Natalia Teaca <lilikoy@sbcglobal.net>
2020-10-02 11:57:06 -0700

    browserRaouter

--- src/index.js
+++ src/index.js
@@ -5,13 +5,13 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'react-mdl/extra/material.css';
 import 'react-mdl/extra/material.js';
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 
 ReactDOM.render(
-  <HashRouter>
+  <BrowserRouter>
     <App />
-  </HashRouter>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
