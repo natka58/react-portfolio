c7ad77845f4bcbc2c07903e6e9385a0a985af5ff
Natalia Teaca <lilikoy@sbcglobal.net>
2020-10-01 23:44:17 -0700

    Updates

---
+++ precache-manifest.95c198e5c95abc0aed1ed0e2f15b353c.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c69f3fcef96787fd1115dc0bece8ef5",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "2aa5811645fd705230a7",
+    "url": "/react-portfolio/static/css/2.cd1c2086.chunk.css"
+  },
+  {
+    "revision": "3ceb29e3c131a9622752",
+    "url": "/react-portfolio/static/css/main.13b70c06.chunk.css"
+  },
+  {
+    "revision": "2aa5811645fd705230a7",
+    "url": "/react-portfolio/static/js/2.76c5cc77.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/react-portfolio/static/js/2.76c5cc77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ceb29e3c131a9622752",
+    "url": "/react-portfolio/static/js/main.78d605b7.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "b6f1dc3e710005c9d3c718cb477a0cbc",
+    "url": "/react-portfolio/static/media/avatar.b6f1dc3e.JPG"
+  },
+  {
+    "revision": "ffa52dace252d8d07d6192fc10fb504c",
+    "url": "/react-portfolio/static/media/covie.ffa52dac.png"
+  },
+  {
+    "revision": "c2d6ed01748bb3fa929f828c5c0e6a92",
+    "url": "/react-portfolio/static/media/note.c2d6ed01.jpg"
+  },
+  {
+    "revision": "a74230520baaad2710b8b68cfb3d6084",
+    "url": "/react-portfolio/static/media/read.a7423052.jpg"
+  },
+  {
+    "revision": "05eaa16aee7ca901b8c8f18ace19ac7e",
+    "url": "/react-portfolio/static/media/resume.05eaa16a.docx"
+  },
+  {
+    "revision": "3adfa0c73972c6e28a818a0009140582",
+    "url": "/react-portfolio/static/media/runbuddy.3adfa0c7.jpg"
+  },
+  {
+    "revision": "93a391ac92a7c1985bf36361e19e5a50",
+    "url": "/react-portfolio/static/media/tech.93a391ac.jpg"
+  },
+  {
+    "revision": "9c651e7e67454b70b56820fcb1f4985b",
+    "url": "/react-portfolio/static/media/trakster.9c651e7e.png"
+  }
+]);
